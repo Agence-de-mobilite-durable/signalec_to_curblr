564c67949e305b8f7f6066771574f2b05e429b38
Antoine Laurent <alaurent@agencemobilitedurable.ca>
2024-11-04 16:28:26 -0500

    fix: remove string

--- cygne/tools/__init__.py
+++ cygne/tools/__init__.py
@@ -1,6 +1,5 @@
 from cygne.tools import geom
 from cygne.tools import ctime
 from cygne.tools import graph
-from cygne.tools import strings
 
-__all__ = ['geom', 'ctime', 'graph', 'strings']
+__all__ = ['geom', 'ctime', 'graph']
